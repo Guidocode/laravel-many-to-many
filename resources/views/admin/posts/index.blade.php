cd94009bf7faee668fbef8562cc562014f69c1c9
Guidocode <guidobuono@icloud.com>
2022-07-11 21:40:50 +0200

    Sistemazione scrollspy post divisi per categorie

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -57,61 +57,57 @@
     {{-- /Posts --}}
 
 
+
     {{-- Post divisi per categorie --}}
-    <div class="bg-info p-2 mb-5">
+    <div class=" bg-success p-2 mb-5">
         <h2 class="title-content">Posts divisi per categorie</h2>
 
+        <nav id="navbar-example2" class="navbar navbar-light bg-light d-flex justify-content-center">
+            <ul class="nav nav-pills">
 
-        <div class="accordion" id="accordionExample">
+                @foreach ($categories as $category)
+                <li class="nav-item">
+                    <a class="nav-link" href="#category{{$loop->iteration}}">{{$category->name}}</a>
+                </li>
+                @endforeach
 
-            @foreach ($categories as $category)
+            </ul>
+        </nav>
 
-                <div class="accordion">
-                <h2 class="accordion-header" id="headingOne">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        {{$category->name}}
-                    </button>
-                </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
-                        <ul class="list-group">
+        <div data-spy="scroll" data-target="#navbar-example2" data-offset="0" class="position-relative" style="height: 400px; overflow-y: scroll;">
+            <ul class="list-group">
 
-                            @forelse ($category->posts as $post)
-                                <li class="list-group-item d-flex justify-content-between align-items-start">
-                                    <div class="ms-2 me-auto fw-bold">
-                                        {{$post->title}}
-                                    </div>
-                                    <div class="d-flex">
-                                        <a class="btn btn-success mx-1" href="{{ route('admin.posts.show', $post) }}">SHOW</a>
-                                        <a class="btn btn-primary mx-1" href="{{ route('admin.posts.edit', $post) }}">EDIT</a>
-                                        <form class="d-inline mx-1"
-                                            onsubmit="return confirm('confermi l\'eliminazione di: {{ $post->title }}?')"
-                                            action="{{ route('admin.posts.destroy', $post) }}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger" >DELETE</button>
-                                        </form>
-                                    </div>
-                                </li>
-                            @empty
-                                <li class="list-group-item d-flex justify-content-between align-items-start">
-                                    <div class="ms-2 me-auto fw-bold">
-                                        Nessun post appartenente a questa categoria
-                                    </div>
-                                </li>
-                            @endforelse
+                @foreach ($categories as $category)
+                    <h3 class="bg-light mt-3 mb-0" id="category{{$loop->iteration}}"><span class="badge bg-info text-dark m-2">{{$category->name}}</span></h3>
 
-                        </ul>
-                    </div>
-                </div>
-                </div>
-            @endforeach
+                    @forelse ($category->posts as $post)
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            <div>
 
+                                <h4>{{$post->title}}</h4>
+                            </div>
+                            <div class="d-flex">
+                                <a class="btn btn-success mx-1" href="{{ route('admin.posts.show', $post) }}">SHOW</a>
+                                <a class="btn btn-primary mx-1" href="{{ route('admin.posts.edit', $post) }}">EDIT</a>
+                                <form class="d-inline mx-1"
+                                    onsubmit="return confirm('confermi l\'eliminazione di: {{ $post->title }}?')"
+                                    action="{{ route('admin.posts.destroy', $post) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger" >DELETE</button>
+                                </form>
+                            </div>
+                        </li>
+                    @empty
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            Nessun post appartenente a questa categoria
+                        </li>
+                    @endforelse
+                @endforeach
+
+            </ul>
         </div>
-
     </div>
     {{-- /Post divisi per categorie --}}
-
-</div>
 
 @endsection
